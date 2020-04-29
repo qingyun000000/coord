b335c140d66503d0e67e3de146df64576aa658a3
qingyun000000 <32904713+qingyun000000@users.noreply.github.com>
2020-04-29 16:43:01 +0800

    初始化coord项目
    初始化coord项目

---
+++ coord/src/main/java/com/zy/coord/CoordApplication.java
@@ -0,0 +1,13 @@
+package com.zy.coord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordApplication.class, args);
+	}
+
+}
---
+++ coord/src/test/java/com/zy/coord/CoordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.coord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
